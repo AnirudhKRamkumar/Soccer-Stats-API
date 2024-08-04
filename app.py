f9a0195b021449aad0ea20888ad5a7da30003434
NinjaHawk2014 <kanirudh541@gmail.com>
2024-08-03 21:39:22 -0400

    starting to build the flask app for visualization

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-import stat_scraper
+# import stat_scraper as ss
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/player')
+def player():
+    return render_template('player_stat_select.html')
+
+@app.route('/squad')
+def squad():
+    return render_template('squad_stat_select.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
