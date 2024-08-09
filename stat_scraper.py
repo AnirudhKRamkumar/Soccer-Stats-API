c0bcfb88c3dba02c316d41f1243e2449a24b80da
NinjaHawk2014 <kanirudh541@gmail.com>
2024-08-08 20:06:23 -0400

    worked with sorting and league specific data

--- stat_scraper.py
+++ stat_scraper.py
@@ -1,17 +1,46 @@
 import pandas as pd
+import time
 
-# Prompt user to input the desired statistic type
+european_leagues = [
+    ("Premier League", "ENG"),
+    ("La Liga", "SPA"),
+    ("Serie A", "ITA"),
+    ("Bundesliga", "GER"),
+    ("Ligue 1", "FRA"),
+    ("UEFA Champions League", "UCL"),
+    ("UEFA Europa League", "UEL", 19),
+    ("UEFA Conference League", "UECL", 882)
+]
 
-def player_stat_display(selected_stat, season):
-# Construct the URL based on the selected statistic
-  if int(season[0:4]) < 2017:
+def comp_stat_display(selected_stat="stats", season=None, comp='EUR'):
+  
+  # setting default arguments for 'season' because the conditions might change from the moment of initialization 
+  if season is None:
+      year = time.gmtime()[0]
+      current_season = year if time.gmtime()[1] > 8 else (year - 1)
+      season = f"{current_season}-{current_season+1}"
+
+  # Some stats weren't tracked until the 2017-2018 season, allow the function to be recalled
+  if int(season[0:4]) < 2017 and selected_stat == "":
     print("Sorry, but this stat wasn't tracked until the 2017-2018 season; only overall, goalkeeping, passing, shooting, and play time stats were tracked!")
     return None
-    
-  url_df = f'https://fbref.com/en/comps/Big5/{season}/{selected_stat}/players/{season}-Big-5-European-Leagues-Stats'
+ 
+  # Find out which competition the user wants to see by using the european_leagues list
+  for item in european_leagues:
+    if comp.upper() == item[1]:
+      comps = [item[2]]
+      break
 
+  #if "EUR" not in comp:
+
+  # Construct the URL based on the selected statistic   
+  url_df = f"https://fbref.com/en/comps/Big5/{season}/{selected_stat}/players/{season}-Big-5-European-Leagues-Stats"
+  print(f"Number of tables found: {len(pd.read_html(url_df))}")
+
+  
   # Read the HTML table from the URL into a list of DataFrames and select the first DataFrame
   df = pd.read_html(url_df)[0]
+  df.to_html('hello.html')
   # Flatten multi-level column headers by joining them with a space and stripping any extra whitespace
   df.columns = [' '.join(col).strip() for col in df.columns]
   # Reset index to ensure it's a standard range index
@@ -60,9 +89,11 @@ def player_stat_display(selected_stat, season):
   # Fill any remaining NaN values in 'League' column with 'Bundesliga'
   df['League'] = df['League'].fillna('Bundesliga')
 
+  a = range_trimming(df, -1, "Bundesliga")
+
   # Print the first few rows of the processed DataFrame
   
-  print(df.head())
+  print(a)
   
   return df
 
@@ -71,7 +102,7 @@ def range_trimming(dataframe, column, condition, comparison=None):
   trimmed = []
   for row in dataframe.iterrows():
     row = row[1]
-    if column in range(len(row)):
+    if abs(column) in range(len(row)):
       if isinstance(condition, int): 
         value = row[column]
         try:
@@ -96,5 +127,4 @@ def range_trimming(dataframe, column, condition, comparison=None):
           continue
   print(trimmed)
 
-df = player_stat_display('stats', '2023-2024')
-range_trimming(df, 1, "ESP")
+df = comp_stat_display()
